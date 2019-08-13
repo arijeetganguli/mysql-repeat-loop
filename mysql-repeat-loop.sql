fb55ecdc386d7b02a0206bfc03b5347b9e055bc7
Arijeet Ganguli <cooldevil.ari@gmail.com>
2019-08-13 10:18:19 +0530

    Create mysql-repeat-loop.sql

---
+++ mysql-repeat-loop.sql
@@ -0,0 +1,25 @@
+CREATE PROCEDURE `Repeat_Loop`(_start INT, _end INT )
+BEGIN
+
+	DECLARE _a INT;
+
+	DROP TEMPORARY TABLE IF EXISTS __table_list;
+	CREATE TEMPORARY TABLE IF NOT EXISTS __table_list 
+	(
+		num INT
+	);
+
+	SET _a = _start;
+
+	REPEAT -- Start repeat
+  
+		INSERT INTO __table_list VALUE (_a);
+    
+		SET _a = _a + 1; -- Counter
+		
+		UNTIL _a > _end -- Condition to continue
+    
+	END REPEAT;
+
+	SELECT * FROM __table_list;
+END
